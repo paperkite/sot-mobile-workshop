b9af543ba2608eb758421ce6a6a806b8468362b8
Farlei Heinen <farleih@gmail.com>
2016-04-21 16:28:01 +1200

    mapping from Storyboard to SnapKit

--- ios/CalcSnapKit/Calc/AboutViewController.swift
+++ ios/CalcSnapKit/Calc/AboutViewController.swift
@@ -7,15 +7,57 @@
 //
 
 import UIKit
+import SnapKit
 
 class AboutViewController: UIViewController {
+    
+    lazy var infoLabel: UILabel = {
+        let infoLabel = UILabel(frame: CGRectZero)
+        infoLabel.numberOfLines = 1
+        infoLabel.font = UIFont.systemFontOfSize(32)
+        infoLabel.text = "iOS Calculator"
+        infoLabel.textColor = UIColor.whiteColor()
+        infoLabel.textAlignment = .Center
+        return infoLabel
+    }()
+    
+    lazy var closeButton: UIButton = {
+        let closeButton = UIButton(type: UIButtonType.Custom)
+        closeButton.backgroundColor = UIColor.lightGrayColor()
+        closeButton.setTitle("Close", forState: .Normal)
+        closeButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        closeButton.addTarget(self, action: #selector(AboutViewController.tapClose(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        return closeButton
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.darkGrayColor()
+        
+        self.view.addSubview(self.infoLabel)
+        self.view.addSubview(self.closeButton)
 
+        self.updateConstraintsWithTraitCollection(self.traitCollection)
+    }
+    
+    func updateConstraintsWithTraitCollection(traitCollection: UITraitCollection) {
+        
+        self.infoLabel.snp_makeConstraints { (make) -> () in
+            make.top.equalTo(self.view).offset(30)
+            make.left.equalTo(self.view)
+            make.right.equalTo(self.view)
+            make.height.equalTo(50)
+        }
+        
+        self.closeButton.snp_makeConstraints { (make) -> () in
+            make.bottom.equalTo(self.view).offset(-30)
+            make.centerX.equalTo(self.view)
+            make.width.equalTo(120)
+            make.height.equalTo(50)
+        }
     }
 
-    @IBAction func tapClose(sender: UIButton) {
+    func tapClose(sender: UIButton) {
         
         self.dismissViewControllerAnimated(true, completion: nil)
     }
--- ios/CalcSnapKit/Calc/ViewController.swift
+++ ios/CalcSnapKit/Calc/ViewController.swift
@@ -7,18 +7,173 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var mainDisplay: UILabel!
-    @IBOutlet weak var valueA: UITextField!
-    @IBOutlet weak var valueB: UITextField!
+    lazy var mainDisplay: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = UIColor.darkGrayColor()
+        label.font = UIFont.systemFontOfSize(35)
+        label.textAlignment = .Right
+        label.textColor = UIColor.whiteColor()
+        
+        return label
+    }()
+    
+    lazy var valueA: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .RoundedRect
+        textField.backgroundColor = UIColor.whiteColor()
+        return textField
+    }()
+    
+    lazy var valueB: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .RoundedRect
+        textField.backgroundColor = UIColor.whiteColor()
+        return textField
+    }()
+    
+    lazy var valueALabel: UILabel = {
+        let valueALabel = UILabel(frame: CGRectZero)
+        valueALabel.numberOfLines = 1
+        valueALabel.text = "value A"
+        return valueALabel
+    }()
+    
+    lazy var valueBLabel: UILabel = {
+        let valueBLabel = UILabel(frame: CGRectZero)
+        valueBLabel.numberOfLines = 1
+        valueBLabel.text = "value B"
+        return valueBLabel
+    }()
+    
+    lazy var stackView : UIStackView = {
+       let stackView = UIStackView()
+        stackView.distribution = .FillEqually
+        stackView.spacing = 3
+        return stackView
+    }()
+    
+    lazy var addButton: UIButton = {
+        let addButton = UIButton(type: UIButtonType.Custom)
+        addButton.backgroundColor = UIColor.darkGrayColor()
+        addButton.titleLabel?.font = UIFont.systemFontOfSize(50)
+        addButton.setTitle("+", forState: .Normal)
+        addButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        addButton.addTarget(self, action: #selector(ViewController.tapAdd(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        return addButton
+    }()
+    
+    
+    lazy var subButton: UIButton = {
+        let subButton = UIButton(type: UIButtonType.Custom)
+        subButton.backgroundColor = UIColor.darkGrayColor()
+        subButton.titleLabel?.font = UIFont.systemFontOfSize(50)
+        subButton.setTitle("-", forState: .Normal)
+        subButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        subButton.addTarget(self, action: #selector(ViewController.tapSub(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        return subButton
+    }()
+    
+    lazy var multButton: UIButton = {
+        let multButton = UIButton(type: UIButtonType.Custom)
+        multButton.titleLabel?.font = UIFont.systemFontOfSize(50)
+        multButton.backgroundColor = UIColor.darkGrayColor()
+        multButton.setTitle("×", forState: .Normal)
+        multButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        multButton.addTarget(self, action: #selector(ViewController.tapMult(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        return multButton
+    }()
+    
+    lazy var divButton: UIButton = {
+        let divButton = UIButton(type: UIButtonType.Custom)
+        divButton.titleLabel?.font = UIFont.systemFontOfSize(50)
+        divButton.backgroundColor = UIColor.darkGrayColor()
+        divButton.setTitle("×", forState: .Normal)
+        divButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        divButton.addTarget(self, action: #selector(ViewController.tapDiv(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        return divButton
+    }()
+    
+    lazy var infoButton: UIButton = {
+        let infoButton = UIButton(type: UIButtonType.InfoDark)
+        infoButton.addTarget(self, action: #selector(ViewController.tapInfo(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        return infoButton
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.view.backgroundColor = UIColor.lightGrayColor()
+        
+        self.view.addSubview(self.mainDisplay)
+        self.view.addSubview(self.valueALabel)
+        self.view.addSubview(self.valueBLabel)
+        self.view.addSubview(self.valueA)
+        self.view.addSubview(self.valueB)
+        self.view.addSubview(self.stackView)
+        self.stackView.addArrangedSubview(self.addButton)
+        self.stackView.addArrangedSubview(self.subButton)
+        self.stackView.addArrangedSubview(self.multButton)
+        self.stackView.addArrangedSubview(self.divButton)
+        self.view.addSubview(self.infoButton)
+        
         self.mainDisplay.text = "0.0"
         self.clearInputs()
+        
+        self.updateConstraintsWithTraitCollection(self.traitCollection)
+    }
+    
+    func updateConstraintsWithTraitCollection(traitCollection: UITraitCollection) {
+        
+        self.mainDisplay.snp_makeConstraints { (make) -> () in
+            make.top.equalTo(self.view).offset(30)
+            make.left.equalTo(self.view).offset(10)
+            make.right.equalTo(self.view).offset(-10)
+            make.height.equalTo(50)
+        }
+        
+        self.valueALabel.snp_makeConstraints { (make) -> () in
+            make.top.equalTo(self.mainDisplay.snp_bottom).offset(10)
+            make.left.equalTo(self.view).offset(10)
+            make.width.equalTo(self.view).multipliedBy(0.4)
+            make.height.equalTo(21)
+        }
+        
+        self.valueBLabel.snp_makeConstraints { (make) -> () in
+            make.top.equalTo(self.mainDisplay.snp_bottom).offset(10)
+            make.right.equalTo(self.view).offset(-10)
+            make.width.equalTo(self.view).multipliedBy(0.4)
+            make.height.equalTo(21)
+        }
+        
+        self.valueA.snp_makeConstraints { (make) -> () in
+            make.top.equalTo(self.valueALabel.snp_bottom)
+            make.left.equalTo(self.valueALabel)
+            make.width.equalTo(self.valueALabel)
+            make.height.equalTo(21)
+        }
+        
+        self.valueB.snp_makeConstraints { (make) -> () in
+            make.top.equalTo(self.valueBLabel.snp_bottom)
+            make.left.equalTo(self.valueBLabel)
+            make.width.equalTo(self.valueBLabel)
+            make.height.equalTo(21)
+        }
+        
+        self.stackView.snp_makeConstraints { (make) -> () in
+            make.top.equalTo(self.valueA.snp_bottom).offset(10)
+            make.left.equalTo(self.view).offset(10)
+            make.right.equalTo(self.view).offset(-10)
+            make.height.equalTo(80)
+        }
+        
+        self.infoButton.snp_makeConstraints { (make) -> () in
+            make.top.equalTo(self.stackView.snp_bottom).offset(10)
+            make.right.equalTo(self.view).offset(-10)
+        }
     }
     
     func clearInputs() {
@@ -28,7 +183,7 @@ class ViewController: UIViewController {
         self.valueA.becomeFirstResponder()
     }
     
-    @IBAction func tapAdd(sender: UIButton) {
+    func tapAdd(sender: UIButton) {
         
         let varA = Double(self.valueA.text ?? "") ?? 0.0
         let varB = Double(self.valueB.text ?? "") ?? 0.0
@@ -36,7 +191,7 @@ class ViewController: UIViewController {
         self.clearInputs()
     }
     
-    @IBAction func tapSub(sender: UIButton) {
+    func tapSub(sender: UIButton) {
         
         let varA = Double(self.valueA.text ?? "") ?? 0.0
         let varB = Double(self.valueB.text ?? "") ?? 0.0
@@ -44,7 +199,7 @@ class ViewController: UIViewController {
         self.clearInputs()
     }
     
-    @IBAction func tapMult(sender: UIButton) {
+    func tapMult(sender: UIButton) {
         
         let varA = Double(self.valueA.text ?? "") ?? 0.0
         let varB = Double(self.valueB.text ?? "") ?? 0.0
@@ -52,12 +207,17 @@ class ViewController: UIViewController {
         self.clearInputs()
     }
     
-    @IBAction func tapDiv(sender: UIButton) {
+    func tapDiv(sender: UIButton) {
         
         let varA = Double(self.valueA.text ?? "") ?? 0.0
         let varB = Double(self.valueB.text ?? "") ?? 0.0
         self.mainDisplay.text = "\(varA / varB)"
         self.clearInputs()
+    }
+    
+    func tapInfo(sender: UIButton) {
+        
+        self.presentViewController(AboutViewController(), animated: true, completion: nil)
     }
 }
 
